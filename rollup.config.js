4e9e75983dd58cbf5a2f3fe501358b0bb938f079
Paul Henschel <drcmda@gmail.com>
2020-03-03 18:23:14 +0100

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 import webWorkerLoader from 'rollup-plugin-web-worker-loader'
 
 const external = ['react', 'react-three-fiber', 'three']
@@ -12,10 +13,6 @@ const getBabelOptions = ({ useESModules }, targets) => ({
   include: ['src/**/*', '**/node_modules/**'],
   runtimeHelpers: true,
   presets: [['@babel/preset-env', { loose: true, modules: false, targets }], '@babel/preset-react'],
-  plugins: [
-    ['transform-react-remove-prop-types', { removeImport: true }],
-    ['@babel/transform-runtime', { regenerator: false, useESModules }],
-  ],
 })
 
 export default [
@@ -24,9 +21,10 @@ export default [
     output: { file: `dist/index.js`, format: 'esm' },
     external,
     plugins: [
-      resolve({ extensions }),
       webWorkerLoader(),
+      resolve({ extensions }),
       babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
+      terser(),
     ],
   },
 ]
