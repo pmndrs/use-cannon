f3bceba2fd3d4e9274723305e5abe04205630e1a
Michael Hemingway <mpghemingway@gmail.com>
2020-03-15 00:03:30 -0400

    Revert "Add multiple shapes to Convex demo"
    This reverts commit 7a1c95dae587b1cec8d74449086f5bddc886c654.

--- examples/src/demos/ConvexPolyhydron.js
+++ examples/src/demos/ConvexPolyhydron.js
@@ -1,28 +1,13 @@
 import * as THREE from 'three'
-import React, { Suspense, useMemo, useState } from 'react'
+import React, { Suspense, useMemo } from 'react'
 import { Canvas, useLoader } from 'react-three-fiber'
 import { Physics, usePlane, useConvexPolyhedron } from 'use-cannon'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry'
 
-// Diamond model by Michael Hemingway (CC-BY-SA)
 function Diamond(props) {
-  // a note on this particular shape: though unlikely in real life,
-  // should this diamond fall undisturbed and completely upright,
-  // it might balance itself perfectly on its bottom vertex.
   const { nodes } = useLoader(GLTFLoader, '/diamond.glb')
-  const [impact, set] = useState(0)
-  const geo = useMemo(() => {
-    let geo = new THREE.Geometry().fromBufferGeometry(nodes.Diamond.geometry)
-    console.log('Diamond Vertices:', geo.vertices.length)
-    // Merge duplicate vertices resulting from glTF export.
-    // Cannon assumes contiguous, closed meshes to work
-    geo.mergeVertices()
-    console.log('Diamond Vertices after merge:', geo.vertices.length)
-    // Ensure loaded mesh is convex and create faces if necessary
-    return new ConvexGeometry(geo.vertices)
-  }, [nodes])
-
+  const geo = useMemo(() => new THREE.Geometry().fromBufferGeometry(nodes.Cylinder.geometry), [])
+  const [impact, set] = React.useState(0)
   const [ref] = useConvexPolyhedron(() => ({
     mass: 100,
     ...props,
@@ -32,34 +17,9 @@ function Diamond(props) {
       setTimeout(() => set(0), 100)
     },
   }))
-  return (
-    <mesh castShadow receiveShadow ref={ref} geometry={geo} dispose={null}>
-      <meshStandardMaterial attach="material" wireframe />
-    </mesh>
-  )
-}
 
-// A cone is a convex shape by definition
-function Cone(props) {
-  const geo = new THREE.ConeGeometry(1, 1, props.sides, 2)
-  geo.mergeVertices()
-  console.log(geo)
-  const [ref] = useConvexPolyhedron(() => ({ mass: 100, ...props, args: geo }))
   return (
-    <mesh castShadow ref={ref} dispose={null}>
-      <coneBufferGeometry attach="geometry" args={[1, 1, props.sides, 2]} />
-      <meshNormalMaterial attach="material" />
-    </mesh>
-  )
-}
-
-// ...And so is a cube!
-function Cube(props) {
-  const geo = new THREE.BoxGeometry(1, 1, 1)
-  const [ref] = useConvexPolyhedron(() => ({ mass: 100, ...props, args: geo }))
-  return (
-    <mesh castShadow ref={ref} dispose={null} receiveShadow>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+    <mesh castShadow ref={ref} geometry={nodes.Cylinder.geometry} dispose={null}>
       <meshNormalMaterial attach="material" />
     </mesh>
   )
@@ -85,16 +45,13 @@ export default () => (
       penumbra={1}
       intensity={2}
       castShadow
-      shadow-mapSize-width={1028}
-      shadow-mapSize-height={1028}
+      shadow-mapSize-width={256}
+      shadow-mapSize-height={256}
     />
     <Suspense fallback={null}>
-      <Physics iterations={40} tolerance={0.004}>
+      <Physics>
         <Plane rotation={[-Math.PI / 2, 0, 0]} />
-        <Diamond position={[0.5, 2, 0]} rotation={[0, 0.1, 0.1]} />
-        <Cube position={[0, 5, 0]} rotation={[0.1, 0.1, 0.1]} sides={6} />
-        <Cone position={[1, 6, 0]} rotation={[0.1, 0.5, 0.1]} sides={8} />
-        <Cone position={[-1, 9, 0]} rotation={[0.1, Math.PI / 3, 0.1]} />
+        <Diamond position={[0, 5, 0]} rotation={[0.1, 0.1, 0.1]} />
       </Physics>
     </Suspense>
   </Canvas>
