ce08d74b367b4ed754a87800b28f17a9b9045160
Paul Henschel <drcmda@gmail.com>
2020-03-17 14:25:00 +0100

    cleanup

--- examples/src/demos/Constraints.js
+++ examples/src/demos/Constraints.js
@@ -4,39 +4,39 @@ import { Physics, useSphere, useBox, useSpring } from 'use-cannon'
 import mergeRefs from 'react-merge-refs'
 
 const Box = React.forwardRef((props, ref) => {
-  const [box] = useBox(() => ({ mass: 1, args: [0.5, 0.5, 0.5], position: [-1, 0, 0] }))
+  const [box] = useBox(() => ({ mass: 1, args: [0.5, 0.5, 0.5], ...props }))
   return (
-    <mesh ref={mergeRefs([box, ref])} position={props.position}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]}></boxBufferGeometry>
+    <mesh ref={mergeRefs([box, ref])}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" />
     </mesh>
   )
 })
 
 const Ball = React.forwardRef((props, ref) => {
-  const [ball, api] = useSphere(() => ({ type: 'Static', mass: 1, args: 0.5, position: [1, 0, 0] }))
-  useFrame(e => api.setPosition((e.mouse.x * e.viewport.width) / 2, (e.mouse.y * e.viewport.height) / 2, 0))
+  const [ball, { setPosition }] = useSphere(() => ({ type: 'Static', mass: 1, args: 0.5, ...props }))
+  useFrame(e => setPosition((e.mouse.x * e.viewport.width) / 2, (e.mouse.y * e.viewport.height) / 2, 0))
   return (
-    <mesh ref={mergeRefs([ball, ref])} position={props.position}>
-      <sphereBufferGeometry attach="geometry" args={[0.5, 64, 64]}></sphereBufferGeometry>
+    <mesh ref={mergeRefs([ball, ref])}>
+      <sphereBufferGeometry attach="geometry" args={[0.5, 64, 64]} />
       <meshStandardMaterial attach="material" />
     </mesh>
   )
 })
 
-const BoxAndBall = props => {
+const BoxAndBall = () => {
   const box = useRef()
   const ball = useRef()
   useSpring(box, ball, { restLength: 1, stiffness: 100, damping: 2 })
   return (
     <>
-      <Box ref={box} position={[1, 0, 0]}></Box>
-      <Ball ref={ball} position={[-1, 0, 0]}></Ball>
+      <Box ref={box} position={[1, 0, 0]} />
+      <Ball ref={ball} position={[-1, 0, 0]} />
     </>
   )
 }
 
-const Test = () => {
+export default () => {
   return (
     <Canvas shadowMap sRGB camera={{ position: [0, 5, 20], fov: 50 }}>
       <color attach="background" args={['#171720']} />
@@ -49,5 +49,3 @@ const Test = () => {
     </Canvas>
   )
 }
-
-export default Test
