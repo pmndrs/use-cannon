90adae61c1784fd4c4a6dd6ec3e9f8788ab625f1
Cody Persinger <codypersinger@gmail.com>
2020-07-06 14:24:22 -0500

    Update KinematicCube example box extents

--- examples/src/demos/KinematicCube.js
+++ examples/src/demos/KinematicCube.js
@@ -15,22 +15,23 @@ function Plane({ color, ...props }) {
 }
 
 function Box() {
-  const [ref, api] = useBox(() => ({ type: 'Kinematic', mass: 1, args: [2, 2, 2] }))
-  useFrame(state => {
+  const boxSize = [4, 4, 4]
+  const [ref, api] = useBox(() => ({ type: 'Kinematic', mass: 1, args: boxSize }))
+  useFrame((state) => {
     const t = state.clock.getElapsedTime()
     api.position.set(Math.sin(t * 2) * 5, Math.cos(t * 2) * 5, 3)
     api.rotation.set(Math.sin(t * 6), Math.cos(t * 6), 0)
   })
   return (
     <mesh ref={ref} castShadow receiveShadow>
-      <boxBufferGeometry attach="geometry" args={[4, 4, 4]} />
+      <boxBufferGeometry attach="geometry" args={boxSize} />
       <meshLambertMaterial attach="material" color="white" side={THREE.DoubleSide} />
     </mesh>
   )
 }
 
 function InstancedSpheres({ number = 100 }) {
-  const [ref] = useSphere(index => ({
+  const [ref] = useSphere((index) => ({
     mass: 1,
     position: [Math.random() - 0.5, Math.random() - 0.5, index * 2],
     args: 1,
