1a9294b36da905749c3cf352b3b69f4e1ab841f0
mattrossman <rossman@gatech.edu>
2020-10-10 23:42:03 -0400

    Demo multiple levels of restitution

--- examples/src/demos/ContactMaterial.js
+++ examples/src/demos/ContactMaterial.js
@@ -1,55 +1,53 @@
 import React from 'react'
 import { Canvas } from 'react-three-fiber'
 import { Physics, useContactMaterial, useMaterial, usePlane, useSphere } from '@react-three/cannon'
-import { Material } from 'cannon-es'
+import { OrbitControls } from '@react-three/drei'
 
-// function Ball(props) {
-// 	const [ref] = useSphere(() => ({ args: 0.5, mass: 1, ...props }))
-// 	return (
-// 		<mesh ref={ref}>
-// 			<sphereBufferGeometry args={[0.5, 16, 16]} />
-// 			<meshStandardMaterial />
-// 		</mesh>
-// 	)
-// }
+function Sphere(props) {
+  const [ref] = useSphere(() => ({ args: 0.5, mass: 10, ...props }))
+  return (
+    <mesh ref={ref} castShadow>
+      <sphereBufferGeometry args={[0.5, 16, 16]} />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
 
-// function Plane(props) {
-// 	const [ref] = usePlane(() => ({ mass: 0, ...props }))
-// 	return (
-// 		<mesh ref={ref}>
-// 			<planeBufferGeometry args={[5, 5]} />
-// 			<meshStandardMaterial color="hotpink" />
-// 		</mesh>
-// 	)
-// }
+function Plane(props) {
+  const [ref] = usePlane(() => ({ ...props }))
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry args={[5, 5]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  )
+}
 
-function Stuff() {
-  const sphereMat = useMaterial(() => 'groundMaterial')
-  const planeMat = useMaterial(() => 'planeMat')
-  console.log(sphereMat)
-  console.log(planeMat)
-  const [sphere] = useSphere(() => ({ args: 0.5, mass: 1, position: [0, 2, 0], material: sphereMat }))
-  const [plane] = usePlane(() => ({ mass: 0, rotation: [-Math.PI / 2, 0, 0], material: planeMat }))
-  useContactMaterial(sphereMat, planeMat, { restitution: 1 })
+function PhysicsContent() {
+  const groundMaterial = useMaterial(() => {})
+  const ballMaterialA = useMaterial(() => {})
+  const ballMaterialB = useMaterial(() => {})
+  const ballMaterialC = useMaterial(() => {})
+  useContactMaterial(ballMaterialA, groundMaterial, { friction: 0.0, restitution: 0.0 })
+  useContactMaterial(ballMaterialB, groundMaterial, { friction: 0.0, restitution: 0.7 })
+  useContactMaterial(ballMaterialC, groundMaterial, { friction: 0.0, restitution: 0.8 })
   return (
     <>
-      <mesh ref={sphere}>
-        <sphereBufferGeometry args={[0.5, 16, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-      <mesh ref={plane}>
-        <planeBufferGeometry args={[5, 5]} />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
+      <Sphere material={ballMaterialA} position={[-1.5, 2, 0]} />
+      <Sphere material={ballMaterialB} position={[0, 2, 0]} />
+      <Sphere material={ballMaterialC} position={[1.5, 2, 0]} />
+      <Plane material={groundMaterial} rotation={[-Math.PI / 2, 0, 0]} />
     </>
   )
 }
 
 export default () => (
-  <Canvas camera={{ position: [3, 3, 3] }}>
-    <pointLight position={[1, 2, 3]} />
-    <Physics>
-      <Stuff />
+  <Canvas shadowMap camera={{ position: [0, 2, 6] }}>
+    <OrbitControls />
+    <pointLight position={[1, 2, 3]} castShadow />
+    <ambientLight intensity={0.2} />
+    <Physics gravity={[0, -10, 0]}>
+      <PhysicsContent />
     </Physics>
   </Canvas>
 )
