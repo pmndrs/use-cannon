5dc3a8b881cac958b2a390f1184451bb4ba85e66
Mike <mpghemingway@gmail.com>
2021-05-28 10:58:24 -0400

    Sync Vehicle demo with Paul's changes

--- examples/src/demos/RaycastVehicle/Vehicle.js
+++ examples/src/demos/RaycastVehicle/Vehicle.js
@@ -5,162 +5,87 @@ import { useRaycastVehicle } from '@react-three/cannon'
 import Chassis from './Chassis'
 import Wheel from './Wheel'
 
-export default function Vehicle(props) {
-  // chassisBody
+function Vehicle({ radius = 0.7, width = 1.2, height = -0.04, front = 1.3, back = -1.15, steer = 0.75, force = 2000, maxBrake = 1e5, ...props }) {
   const chassis = useRef()
-  // wheels
-  const wheels = []
-  const wheelInfos = []
-
-  // chassis - wheel connection helpers
-  const chassisWidth = 1.2
-  const chassisHeight = -0.04 // ground clearance
-  const chassisFront = 1.3
-  const chassisBack = -1.15
+  const wheel1 = useRef()
+  const wheel2 = useRef()
+  const wheel3 = useRef()
+  const wheel4 = useRef()
 
   const wheelInfo = {
-    radius: props.wheelRadius || 0.7,
-    directionLocal: [0, -1, 0], // same as Physics gravity
+    radius,
+    directionLocal: [0, -1, 0], // set to same as Physics Gravity
     suspensionStiffness: 30,
     suspensionRestLength: 0.3,
     maxSuspensionForce: 1e4,
     maxSuspensionTravel: 0.3,
-    dampingRelaxation: 2.3,
+    dampingRelaxation: 10,
     dampingCompression: 4.4,
-    frictionSlip: 5,
-    rollInfluence: 0.01,
-    axleLocal: [-1, 0, 0], // wheel rotates around X-axis, invert if wheels rotate the wrong way
+    axleLocal: [-1, 0, 0], // This is inverted for asymmetrical wheel models (left v. right sided)
     chassisConnectionPointLocal: [1, 0, 1],
-    isFrontWheel: false,
     useCustomSlidingRotationalSpeed: true,
     customSlidingRotationalSpeed: -30,
+    frictionSlip: 2
   }
 
-  // FrontLeft [-X,Y,Z]
-  const wheel_1 = useRef()
-  const wheelInfo_1 = { ...wheelInfo }
-  wheelInfo_1.chassisConnectionPointLocal = [-chassisWidth / 2, chassisHeight, chassisFront]
-  wheelInfo_1.isFrontWheel = true
-
-  // FrontRight [X,Y,Z]
-  const wheel_2 = useRef()
-  const wheelInfo_2 = { ...wheelInfo }
-  wheelInfo_2.chassisConnectionPointLocal = [chassisWidth / 2, chassisHeight, chassisFront]
-  wheelInfo_2.isFrontWheel = true
-
-  // BackLeft [-X,Y,-Z]
-  const wheel_3 = useRef()
-  const wheelInfo_3 = { ...wheelInfo }
-  wheelInfo_3.isFrontWheel = false
-  wheelInfo_3.chassisConnectionPointLocal = [-chassisWidth / 2, chassisHeight, chassisBack]
-
-  // BackRight [X,Y,-Z]
-  const wheel_4 = useRef()
-  const wheelInfo_4 = { ...wheelInfo }
-  wheelInfo_4.chassisConnectionPointLocal = [chassisWidth / 2, chassisHeight, chassisBack]
-  wheelInfo_4.isFrontWheel = false
-
-  wheels.push(wheel_1, wheel_2, wheel_3, wheel_4)
-  wheelInfos.push(wheelInfo_1, wheelInfo_2, wheelInfo_3, wheelInfo_4)
+  const wheelInfo1 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [-width / 2, height, front] }
+  const wheelInfo2 = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [width / 2, height, front] }
+  const wheelInfo3 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [-width / 2, height, back] }
+  const wheelInfo4 = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [width / 2, height, back] }
 
   const [vehicle, api] = useRaycastVehicle(() => ({
     chassisBody: chassis,
-    wheels,
-    wheelInfos,
+    wheels: [wheel1, wheel2, wheel3, wheel4],
+    wheelInfos: [wheelInfo1, wheelInfo2, wheelInfo3, wheelInfo4],
     indexForwardAxis: 2,
     indexRightAxis: 0,
-    indexUpAxis: 1,
+    indexUpAxis: 1
   }))
 
-  const forward = useKeyPress('w')
-  const backward = useKeyPress('s')
-  const left = useKeyPress('a')
-  const right = useKeyPress('d')
-  const brake = useKeyPress(' ') // space bar
-  const reset = useKeyPress('r')
-
-  const [steeringValue, setSteeringValue] = useState(0)
-  const [engineForce, setEngineForce] = useState(0)
-  const [brakeForce, setBrakeForce] = useState(0)
-
-  const maxSteerVal = 0.5
-  const maxForce = 1e3
-  const maxBrakeForce = 1e5
+  const keys = useRef({ forward: false, backward: false, left: false, right: false, brake: false, reset: false })
+  useKeyPress(['ArrowUp', 'w'], (pressed) => (keys.current.forward = pressed))
+  useKeyPress(['ArrowDown', 's'], (pressed) => (keys.current.backward = pressed))
+  useKeyPress(['ArrowLeft', 'a'], (pressed) => (keys.current.left = pressed))
+  useKeyPress(['ArrowRight', 'd'], (pressed) => (keys.current.right = pressed))
+  useKeyPress([' '], (pressed) => (keys.current.brake = pressed))
+  useKeyPress(['r'], (pressed) => (keys.current.reset = pressed))
 
   useFrame(() => {
-    if (left && !right) {
-      setSteeringValue(maxSteerVal)
-    } else if (right && !left) {
-      setSteeringValue(-maxSteerVal)
-    } else {
-      setSteeringValue(0)
-    }
-    if (forward && !backward) {
-      setBrakeForce(0)
-      setEngineForce(-maxForce)
-    } else if (backward && !forward) {
-      setBrakeForce(0)
-      setEngineForce(maxForce)
-    } else if (engineForce !== 0) {
-      setEngineForce(0)
-    }
-    if (brake) {
-      setBrakeForce(maxBrakeForce)
-    }
-    if (!brake) setBrakeForce(0)
+    const { forward, backward, left, right, brake, reset } = keys.current
+    for (let e = 2; e < 4; e++) api.applyEngineForce(forward || backward ? force * (forward && !backward ? -1 : 1) : 0, 2)
+    for (let s = 0; s < 2; s++) api.setSteeringValue(left || right ? steer * (left && !right ? 1 : -1) : 0, s)
+    for (let b = 2; b < 4; b++) api.setBrake(brake ? maxBrake : 0, b)
     if (reset) {
-      chassis.current.api.position.set(0, 5, 0)
+      chassis.current.api.position.set(0, 0.5, 0)
       chassis.current.api.velocity.set(0, 0, 0)
       chassis.current.api.angularVelocity.set(0, 0.5, 0)
       chassis.current.api.rotation.set(0, -Math.PI / 4, 0)
     }
   })
 
-  useEffect(() => {
-    api.applyEngineForce(engineForce, 2)
-    api.applyEngineForce(engineForce, 3)
-  }, [engineForce])
-  useEffect(() => {
-    api.setSteeringValue(steeringValue, 0)
-    api.setSteeringValue(steeringValue, 1)
-  }, [steeringValue])
-  useEffect(() => {
-    wheels.forEach((_, i) => api.setBrake(brakeForce, i))
-  }, [brakeForce])
-
   return (
-    <group ref={vehicle}>
-      <Chassis
-        ref={chassis}
-        rotation={props.rotation}
-        position={props.position}
-        angularVelocity={props.angularVelocity}
-      />
-      <Wheel ref={wheel_1} size={props.wheelRadius || 0.7} leftSide />
-      <Wheel ref={wheel_2} size={props.wheelRadius || 0.7} />
-      <Wheel ref={wheel_3} size={props.wheelRadius || 0.7} leftSide />
-      <Wheel ref={wheel_4} size={props.wheelRadius || 0.7} />
+    <group ref={vehicle} position={[0, -0.4, 0]}>
+      <Chassis ref={chassis} rotation={props.rotation} position={props.position} angularVelocity={props.angularVelocity} />
+      <Wheel ref={wheel1} radius={radius} leftSide />
+      <Wheel ref={wheel2} radius={radius} />
+      <Wheel ref={wheel3} radius={radius} leftSide />
+      <Wheel ref={wheel4} radius={radius} />
     </group>
   )
 }
 
-function useKeyPress(target) {
-  const [keyPressed, setKeyPressed] = useState(false)
+export default Vehicle
 
-  // If pressed key is our target key then set to true
-  const downHandler = ({ key }) => (key === target ? setKeyPressed(true) : null)
-  const upHandler = ({ key }) => (key === target ? setKeyPressed(false) : null)
 
-  // Add event listeners
+function useKeyPress(target, event) {
   useEffect(() => {
+    const downHandler = ({ key }) => target.indexOf(key) !== -1 && event(true)
+    const upHandler = ({ key }) => target.indexOf(key) !== -1 && event(false)
     window.addEventListener('keydown', downHandler)
     window.addEventListener('keyup', upHandler)
-    // Remove event listeners on cleanup
     return () => {
       window.removeEventListener('keydown', downHandler)
       window.removeEventListener('keyup', upHandler)
     }
-  }, []) // Empty array ensures that effect is only run on mount and unmount
-
-  return keyPressed
+  }, [])
 }
--- examples/src/demos/RaycastVehicle/index.js
+++ examples/src/demos/RaycastVehicle/index.js
@@ -1,25 +1,20 @@
-import React from 'react'
+// This demo is also playable without installation here:
+// https://codesandbox.io/s/basic-demo-forked-ebr0x
+
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Physics, useCylinder, usePlane } from '@react-three/cannon'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Environment } from '@react-three/drei'
 
 import Vehicle from './Vehicle'
 
 function Plane(props) {
-  const [ref] = usePlane(() => ({
-    type: 'Static',
-    material: 'ground',
-    ...props,
-  }))
+  const [ref] = usePlane(() => ({ type: 'Static', material: 'ground', ...props }))
   return (
     <group ref={ref}>
-      <mesh>
-        <planeBufferGeometry args={[15, 15]} />
-        <meshBasicMaterial color="#ffb385" />
-      </mesh>
       <mesh receiveShadow>
-        <planeBufferGeometry args={[15, 15]} />
-        <shadowMaterial color="lightsalmon" />
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="#303030" />
       </mesh>
     </group>
   )
@@ -40,31 +35,25 @@ function Pillar(props) {
   )
 }
 
-const defaultContactMaterial = {
-  contactEquationRelaxation: 4,
-  friction: 1e-3,
-}
-
 const VehicleScene = () => {
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 5, 20], fov: 50 }}>
-        <OrbitControls />
+      <Canvas shadows camera={{ position: [0, 5, 15], fov: 50 }}>
+        <fog attach="fog" args={['#171720', 10, 50]} />
         <color attach="background" args={['#171720']} />
         <ambientLight intensity={0.1} />
-        <spotLight position={[10, 10, 10]} angle={0.3} intensity={1} castShadow penumbra={0.5} />
-        <Physics broadphase="SAP" {...defaultContactMaterial} allowSleep>
+        <spotLight position={[10, 10, 10]} angle={0.5} intensity={1} castShadow penumbra={1} />
+        <Physics broadphase="SAP" contactEquationRelaxation={4} friction={1e-3} allowSleep>
           <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
-          <Vehicle
-            position={[0, 5, 0]}
-            rotation={[0, -Math.PI / 4, 0]}
-            angularVelocity={[0, 0.5, 0]} // to get you rolling
-            wheelRadius={0.3}
-          />
+          <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} wheelRadius={0.3} />
           <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
           <Pillar position={[0, 2.5, -5]} userData={{ id: 'pillar-2' }} />
           <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} />
         </Physics>
+        <Suspense fallback={null}>
+          <Environment preset="night" />
+        </Suspense>
+        <OrbitControls />
       </Canvas>
       <div
         style={{
