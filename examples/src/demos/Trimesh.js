a6ec25fac31c8653e7fad95700e5acdf4e95eaa8
Mike <mikemingway@gmail.com>
2021-06-27 18:26:02 -0400

    Integrate pausing functionality
    Note, as suspected, camera controls invalidate, invalidating the pausing functionality

--- examples/src/demos/Trimesh.js
+++ examples/src/demos/Trimesh.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useState, useEffect } from 'react'
+import { Canvas, invalidate } from '@react-three/fiber'
 import { Physics, useSphere, useTrimesh } from '@react-three/cannon'
 import { OrbitControls, TorusKnot, useGLTF } from '@react-three/drei'
+import create from 'zustand'
+
+const [useStore] = create((set) => ({
+  isPaused: false,
+  pause: () => set({ isPaused: true }),
+  play: () => set({ isPaused: false }),
+}))
+
+function Controls() {
+  const { isPaused, pause, play } = useStore()
+  return (
+    <div>
+      <span>Paused? {isPaused ? 'Yes' : 'No'}</span> <br /> <br />
+      <button onClick={pause}>pause</button>
+      <button onClick={play}>resume</button>
+    </div>
+  )
+}
 
 const WeirdCheerio = (props) => {
   const [ref] = useSphere(() => ({ mass: 1, args: props.radius, ...props }))
@@ -20,6 +38,7 @@ const Bowl = (props) => {
   const indices = geometry.index.array
 
   const [hovered, setHover] = useState(false)
+  const { isPaused } = useStore()
 
   const [ref] = useTrimesh(() => ({
     mass: 0,
@@ -27,25 +46,48 @@ const Bowl = (props) => {
     args: [vertices, indices],
   }))
 
+  useEffect(() => {
+    if (!isPaused) invalidate()
+  }, [isPaused])
+
   return (
-    <mesh ref={ref} geometry={nodes.bowl.geometry} onPointerOver={() => setHover(true)} onPointerOut={() => setHover(false)} {...props}>
+    <mesh
+      ref={ref}
+      geometry={nodes.bowl.geometry}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+      {...props}>
       <meshStandardMaterial color={'white'} wireframe={hovered} />
     </mesh>
   )
 }
 
 export default () => (
-  <Canvas shadows>
-    <color attach="background" args={['#171720']} />
-    <ambientLight intensity={0.3} />
-    <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
-    <OrbitControls />
-    <Physics>
-      <Bowl rotation={[0, 0, 0]} />
-      <WeirdCheerio radius={0.1} position={[0.3, 11, 0]} />
-      <WeirdCheerio radius={0.1} position={[0, 10, 0]} />
-      <WeirdCheerio radius={0.1} position={[0.4, 9, 0.7]} />
-      <WeirdCheerio radius={0.1} position={[0.2, 13, 1]} />
-    </Physics>
-  </Canvas>
+  <>
+    <Canvas shadows frameloop="demand">
+      <color attach="background" args={['#171720']} />
+      <ambientLight intensity={0.3} />
+      <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+      <OrbitControls />
+      <Physics shouldInvalidate={false}>
+        <Bowl rotation={[0, 0, 0]} />
+        <WeirdCheerio radius={0.1} position={[0.3, 11, 0]} />
+        <WeirdCheerio radius={0.1} position={[0, 10, 0]} />
+        <WeirdCheerio radius={0.1} position={[0.4, 9, 0.7]} />
+        <WeirdCheerio radius={0.1} position={[0.2, 13, 1]} />
+      </Physics>
+    </Canvas>
+    <div
+      style={{
+        position: 'absolute',
+        top: 20,
+        left: 50,
+        color: 'white',
+        fontSize: '1.2em',
+      }}>
+      <pre>
+        <Controls />
+      </pre>
+    </div>
+  </>
 )
