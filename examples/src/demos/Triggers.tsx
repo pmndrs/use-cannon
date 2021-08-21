d3855448d244f26f0e7d77e49cd3a41565684a45
Bjorn Stromberg <bjorn@bjornstar.com>
2021-08-21 11:05:05 +0900

    Convert Triggers example to typescript

--- examples/src/demos/Triggers.tsx
+++ examples/src/demos/Triggers.tsx
@@ -1,13 +1,14 @@
+import type { BoxProps, PlaneProps } from '@react-three/cannon'
 import { Physics, useBox, usePlane, useSphere } from '@react-three/cannon'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useState } from 'react'
 
-function BoxTrigger({ onCollide, size, position }) {
-  const [ref] = useBox(() => ({ isTrigger: true, args: size, position, onCollide }))
+function BoxTrigger({ args, onCollide, position }: BoxProps) {
+  const [ref] = useBox(() => ({ isTrigger: true, args, position, onCollide }))
   return (
-    <mesh ref={ref} position={position}>
-      <boxBufferGeometry args={size} />
+    <mesh {...{ position, ref }}>
+      <boxBufferGeometry args={args} />
       <meshStandardMaterial wireframe color="green" />
     </mesh>
   )
@@ -27,7 +28,7 @@ function Ball() {
   )
 }
 
-function Plane(props) {
+function Plane(props: PlaneProps) {
   const [ref] = usePlane(() => ({ type: 'Static', ...props }))
   return (
     <mesh ref={ref} receiveShadow>
@@ -40,7 +41,7 @@ function Plane(props) {
 export default () => {
   const [bg, setbg] = useState('#171720')
   return (
-    <Canvas shadows camera={{ position: [-10, 15, 5], material: { restitution: 10 }, fov: 50 }}>
+    <Canvas shadows camera={{ position: [-10, 15, 5], fov: 50 }}>
       <OrbitControls />
       <fog attach="fog" args={[bg, 10, 50]} />
       <color attach="background" args={[bg]} />
@@ -48,12 +49,12 @@ export default () => {
       <spotLight position={[10, 10, 10]} angle={0.5} intensity={1} castShadow penumbra={1} />
       <Physics>
         <BoxTrigger
+          args={[4, 1, 4]}
           onCollide={(e) => {
             console.log('Collision event on BoxTrigger', e)
             setbg('#fe4365')
           }}
           position={[0, 5, 0]}
-          size={[4, 1, 4]}
         />
         <Ball />
         <Plane rotation={[-Math.PI / 2, 0, 0]} />
