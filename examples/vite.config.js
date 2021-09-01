bc491c2990ca1c401196029bae32bdd406ff30f9
Bjorn Stromberg <bjorn@bjornstar.com>
2021-09-01 22:02:01 +0900

    [CI] Test on node v14 as vercel doesn't support 16 yet

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from 'node:path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import reactJsx from 'vite-react-jsx'
