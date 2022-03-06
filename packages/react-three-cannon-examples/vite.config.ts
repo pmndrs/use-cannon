703b58c74de5d684e9651e50b29f91c051812cb6
Bjorn Stromberg <bjorn@bjornstar.com>
2022-03-06 14:27:17 +0100

    Remove react-dom from peer dependencies, fix vite resolving issues, just use yarn to install

--- packages/react-three-cannon-examples/vite.config.ts
+++ packages/react-three-cannon-examples/vite.config.ts
@@ -1,15 +1,7 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import reactJsx from 'vite-react-jsx'
 
 export default defineConfig({
   plugins: [reactJsx(), reactRefresh()],
-  resolve: {
-    alias: {
-      // Resolve symlink ourselves
-      '@react-three/fiber': resolve('..', '..', 'node_modules', '@react-three', 'fiber'),
-      three: resolve('..', '..', 'node_modules', 'three'),
-    },
-  },
 })
