a5831d75aa9fd91573410417046c6b579ea396f4
Bjorn Stromberg <bjorn@bjornstar.com>
2022-04-03 20:01:26 +0200

    Use react-dom/client for react 18

--- packages/react-three-cannon-examples/src/index.tsx
+++ packages/react-three-cannon-examples/src/index.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+createRoot(document.getElementById('root')!).render(<App />)
