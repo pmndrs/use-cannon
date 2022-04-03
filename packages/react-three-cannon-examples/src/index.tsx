ff45cde4d4c5526a136f63c03486dd889ce22302
Bjorn Stromberg <bjorn@bjornstar.com>
2022-04-03 22:05:37 +0200

    fix lint & fix lint-staged

--- packages/react-three-cannon-examples/src/index.tsx
+++ packages/react-three-cannon-examples/src/index.tsx
@@ -2,4 +2,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(<App />)
+const root = document.getElementById('root')
+
+if (root) {
+  createRoot(root).render(<App />)
+}
