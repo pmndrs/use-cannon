7f5c5ef6c960fc3d47a8e6ec375fdbfee975fcea
Bjorn Stromberg <bjorn@bjornstar.com>
2022-04-04 09:34:23 +0200

    Use react-dom/client for react 18 (#371)
    * Use react-dom/client for react 18
    
    * fix lint & fix lint-staged

--- packages/react-three-cannon-examples/src/index.tsx
+++ packages/react-three-cannon-examples/src/index.tsx
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+
+if (root) {
+  createRoot(root).render(<App />)
+}
